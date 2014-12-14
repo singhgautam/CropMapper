73f8c1daa335258b48679567bdbb7a930d7c569f
Gautam Singh <singh.gautam.iitg@gmail.com>
2014-12-14 20:33:25 +0530

    Wrote sandbox to test predicateTOSubjectObject().

--- CropMapper/src/main/java/com/gautam/CropMapper/App.java
+++ CropMapper/src/main/java/com/gautam/CropMapper/App.java
@@ -1,9 +1,17 @@
 package com.gautam.CropMapper;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Sparql enDBpedia = new Sparql("http://ko.dbpedia.org/sparql");
+        String predicateURI = "http://ko.dbpedia.org/property/작곡";
+        Map<String, String> outMap = enDBpedia.predicateToSubjectObject(predicateURI);
+        for(Entry<String, String> thisEntry : outMap.entrySet()){
+        	System.out.println(thisEntry.getKey()+" :: "+thisEntry.getValue());
+        }
     }
 }
